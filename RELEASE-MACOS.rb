fbf4310b5ace96c3168bd68dd64ae3b371f176f6
soramimi <fi7s-fct@asahi-net.or.jp>
2017-04-30 17:16:26 +0900

    macOS用リリース作成スクリプトを修正

--- RELEASE-MACOS.rb
+++ RELEASE-MACOS.rb
@@ -15,3 +15,5 @@ FileUtils.cp("../_build_AskPass_Release/askpass", "#{$workdir}/#{$product_name}.
 Dir.chdir($workdir) {
 	`zip -r Guitar-#{$version_a}.#{$version_b}.#{$version_c}-macos.zip Guitar.app`
 }
+
+`/opt/Qt5.8.0/5.8/clang_64/bin/macdeployqt #{$workdir}/#{$product_name}.app`
