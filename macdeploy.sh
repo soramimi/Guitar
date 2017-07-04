97b052b0a1a66b56ab4880c8000ad8e911ae7f35
soramimi <fi7s-fct@asahi-net.or.jp>
2017-07-04 23:47:08 +0900

    スクリプトとcopyrightの調整

--- macdeploy.sh
+++
@@ -1,1 +0,0 @@
-/opt/Qt/5.7/clang_64/bin/macdeployqt ../_build_Guitar_Release/Guitar.app
--- version.rb
+++ version.rb
@@ -3,11 +3,14 @@
 $product_name = "Guitar"
 $copyright_year = 2017
 $version_a = 0
-$version_b = 1
+$version_b = 2
 $version_c = 0
 $version_d = 0
 
-# v0.1.0
+# v0.2.0 xxxx-xx-xx
+#	* 
+#
+# v0.1.0 2017-05-03
 #	* 
 #
 # v0.0.0 2016-12-25
