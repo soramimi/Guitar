501953f0fa1a32c839a3d772fddfe66bdbfa9cae
soramimi <fi7s-fct@asahi-net.or.jp>
2019-07-15 11:42:04 +0900

    fetch and pull with --tags

--- src/Git.cpp
+++ src/Git.cpp
@@ -971,12 +971,12 @@ void Git::unstage(QStringList const &paths)
 
 void Git::pull(AbstractPtyProcess *pty)
 {
-	git("pull", true, false, pty);
+	git("pull --tags", true, false, pty);
 }
 
 void Git::fetch(AbstractPtyProcess *pty, bool prune)
 {
-	QString cmd = "fetch";
+	QString cmd = "fetch --tags";
 	if (prune) {
 		cmd += " --prune";
 	}
