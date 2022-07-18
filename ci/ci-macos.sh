b99a3185b4d17098c9c0472dadc908cae203d89d
soramimi <fi7s-fct@asahi-net.or.jp>
2022-07-18 18:42:40 +0900

    for mac os

---
+++ ci/ci-macos.sh
@@ -0,0 +1,16 @@
+mkdir _bin
+cp /Users/soramimi/develop/Guitar/_bin/libz.a _bin/
+cd packaging/mac
+rm -fr build
+mkdir build
+cd build
+/opt/Qt/5.15.0/clang_64/bin/qmake ../../../Guitar.pro
+make -j10
+cd ..
+rm -fr Guitar.app
+mv ../../_bin/Guitar.app .
+/opt/Qt/5.15.0/clang_64/bin/macdeployqt Guitar.app
+rm -f Guitar-macos.zip
+zip -r Guitar-macos.zip Guitar.app
+curl -T Guitar-macos.zip ftp://10.10.10.5/Public/pub/nightlybuild/
+
