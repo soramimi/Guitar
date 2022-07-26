5469976b44489ee1aef9b03ca27da34d0443f24f
soramimi <fi7s-fct@asahi-net.or.jp>
2022-07-26 21:03:40 +0900

    test

--- packaging/mac/mk.sh
+++ packaging/mac/mk.sh
@@ -10,4 +10,7 @@ mv ../../_bin/Guitar.app .
 /opt/Qt/6.2.4/macos/bin/macdeployqt Guitar.app
 rm Guitar-macos.zip
 zip -r Guitar-macos.zip Guitar.app
+date
+ls
+ping 10.10.10.5 -c 5
 curl -T Guitar-macos.zip ftp://10.10.10.5/Public/pub/nightlybuild/
