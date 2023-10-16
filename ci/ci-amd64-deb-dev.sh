f7b0091c64ef6381b956284fcd61c9cff9c389df
soramimi <fi7s-fct@asahi-net.or.jp>
2023-10-17 02:03:26 +0900

    dev testing

---
+++ ci/ci-amd64-deb-dev.sh
@@ -0,0 +1,15 @@
+rm _bin -fr
+rm _build -fr
+mkdir _bin
+ruby prepare.rb
+cd packaging/deb
+mkdir _build
+cd _build
+qmake "CONFIG+=release" ../../../Guitar.pri
+make -j2
+cd ..
+cp _build/Guitar .
+ruby mk-deb.rb
+file=`./debname.rb`
+curl -T $file ftp://10.0.0.5/Public/pub/_tmp/a.bin
+
