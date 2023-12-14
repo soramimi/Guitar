8d7d82acda53b0905a2e819513e5cca324ad55e3
soramimi <fi7s-fct@asahi-net.or.jp>
2023-12-15 03:11:34 +0900

    .

--- ci/ci-amd64-deb-dev.sh
+++ ci/ci-amd64-deb-dev.sh
@@ -9,13 +9,6 @@ qmake "CONFIG+=release" ../../../Guitar.pri
 make -j2
 cd ..
 cp _build/Guitar .
-<<<<<<< HEAD
-ruby mk-deb.rb
-file=`./debname.rb`
-curl -T $file ftp://10.0.0.5/Public/pub/_tmp/a.bin
-
-=======
 SUFFIX=-dev ruby mk-deb.rb
 file=`SUFFIX=-dev ./debname.rb`
 curl -T $file ftp://10.0.0.5/Public/pub/_tmp/
->>>>>>> 919d5e7064918feb5ad3fb5dcbc2ce515c6dcc02
