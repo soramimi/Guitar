6925a0335ee6162e5683e7369fcc39352459adbd
soramimi <fi7s-fct@asahi-net.or.jp>
2024-05-16 01:03:40 +0900

    KDE環境で.gitignore候補に勝手にアクセラレーションキーが設定されることがある問題に対処

--- src/EditGitIgnoreDialog.cpp
+++ src/EditGitIgnoreDialog.cpp
@@ -16,22 +16,30 @@ EditGitIgnoreDialog::EditGitIgnoreDialog(MainWindow *parent, QString const &giti
 	setWindowFlags(flags);
 
 	QFileInfo info(file);
-
-	ui->radioButton_1->setText(file);
-	ui->radioButton_2->setText("*." + info.suffix());
+	
+	ui->radioButton_1->setVisible(false);
+	ui->radioButton_2->setVisible(false);
+	ui->radioButton_3->setVisible(false);
+	ui->radioButton_4->setVisible(false);
+	
+	auto SetText = [](QRadioButton *button, QString const &text) {
+		if (!text.isEmpty()) {
+		button->setText(text);
+		button->setObjectName(text);
+		button->setVisible(true);
+		}
+	};
+	
+	SetText(ui->radioButton_1, file);
+	SetText(ui->radioButton_2, "*." + info.suffix());
 
 	int i = file.indexOf('/');
 	if (i > 0) {
-		ui->radioButton_3->setText(file.mid(0, i + 1));
+		SetText(ui->radioButton_3, file.mid(0, i + 1));
 		int j = file.lastIndexOf('/');
 		if (i < j) {
-			ui->radioButton_4->setText(file.mid(0, j + 1));
-		} else {
-			ui->radioButton_4->setVisible(false);
+			SetText(ui->radioButton_4, file.mid(0, j + 1));
 		}
-	} else {
-		ui->radioButton_3->setVisible(false);
-		ui->radioButton_4->setVisible(false);
 	}
 
 	ui->radioButton_1->click();
@@ -49,10 +57,10 @@ MainWindow *EditGitIgnoreDialog::mainwindow()
 
 QString EditGitIgnoreDialog::text() const
 {
-	if (ui->radioButton_1->isChecked()) return ui->radioButton_1->text();
-	if (ui->radioButton_2->isChecked()) return ui->radioButton_2->text();
-	if (ui->radioButton_3->isChecked()) return ui->radioButton_3->text();
-	if (ui->radioButton_4->isChecked()) return ui->radioButton_4->text();
+	if (ui->radioButton_1->isChecked()) return ui->radioButton_1->objectName();
+	if (ui->radioButton_2->isChecked()) return ui->radioButton_2->objectName();
+	if (ui->radioButton_3->isChecked()) return ui->radioButton_3->objectName();
+	if (ui->radioButton_4->isChecked()) return ui->radioButton_4->objectName();
 	return QString();
 }
 
