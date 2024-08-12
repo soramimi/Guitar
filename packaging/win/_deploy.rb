99e84ead49cf562ee92d58c880cac6868b71107e
soramimi <fi7s-fct@asahi-net.or.jp>
2024-08-13 05:43:33 +0900

    change Qt version 6.6.3 to 6.7.2

--- packaging/win/_deploy.rb
+++ packaging/win/_deploy.rb
@@ -2,6 +2,8 @@
 require 'fileutils'
 $script_dir = __dir__
 
+$qtver = "6.7.2"
+
 if $suffix == nil then
 	$suffix = ""
 else
@@ -28,7 +30,7 @@ FileUtils.rm_rf "zlib"
 run "git clone https://github.com/madler/zlib"
 FileUtils.cp "../../zlib.pro", "zlib/"
 Dir.chdir("zlib") {
-	run "C:/Qt/6.6.3/msvc2019_64/bin/qmake.exe CONFIG+=release zlib.pro"
+	run "C:/Qt/\{$qtver}/msvc2019_64/bin/qmake.exe CONFIG+=release zlib.pro"
 	run "C:/Qt/Tools/QtCreator/bin/jom/jom.exe"
 }
 ENV["INCLUDE"] = $script_dir + "/zlib;" + ENV["INCLUDE"]
@@ -40,7 +42,7 @@ run "ruby prepare.rb"
 FileUtils.cp $script_dir + "/zlib/_bin/libz.lib", "_bin/"
 
 mkcd $script_dir + "/build"
-run "C:/Qt/6.6.3/msvc2019_64/bin/qmake.exe CONFIG+=release ../../../Guitar.pro"
+run "C:/Qt/\{$qtver}/msvc2019_64/bin/qmake.exe CONFIG+=release ../../../Guitar.pro"
 run "C:/Qt/Tools/QtCreator/bin/jom/jom.exe"
 
 Dir.chdir $script_dir + "/../../"
