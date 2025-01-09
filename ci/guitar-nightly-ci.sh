5d5ab5db614fefa130d6b7b132c362ecf4351abb
soramimi <fi7s-fct@asahi-net.or.jp>
2025-01-09 13:00:51 +0900

    fix script

--- ci/guitar-nightly-ci.sh
+++ ci/guitar-nightly-ci.sh
@@ -6,10 +6,11 @@ pushd _guitar-nightly-ci
 git clone git@github.com:soramimi/Guitar.git
 cd Guitar/docker
 make build
-make guitar-deb
-make guitar-appimage
+#make guitar-deb
+#make guitar-appimage
+make guitar-doxy
 popd
-#cp _guitar-nightly-ci/Guitar/docker/home/guitar_*.deb /mnt/lucy/pub/nightlybuild/test/
-#cp _guitar-nightly-ci/Guitar/docker/home/Guitar-*.AppImage /mnt/lucy/pub/nightlybuild/test/
+
 curl -T _guitar-nightly-ci/Guitar/docker/home/guitar_*.deb ftp://192.168.0.5/Public/pub/nightlybuild/test/
 curl -T _guitar-nightly-ci/Guitar/docker/home/Guitar-*.AppImage ftp://192.168.0.5/Public/pub/nightlybuild/test/
+curl -T _guitar-nightly-ci/Guitar/doxy/guitar-doxy-html.tar.zst ftp://192.168.0.5/Public/pub/nightlybuild/
