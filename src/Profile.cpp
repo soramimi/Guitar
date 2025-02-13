3d7cb27df3bcd7b295766e2fd6bee6a11f0e6131
soramimi <fi7s-fct@asahi-net.or.jp>
2025-02-13 12:03:29 +0900

    Move inline log function to Profile.cpp from Profile.h

--- src/Profile.cpp
+++ src/Profile.cpp
@@ -1,3 +1,19 @@
 #include "Profile.h"
+#include <QDebug>
 
+void Profile::log(const QString &s)
+{
+	qDebug() << s;
+}
 
+Profile::Profile(const char *func)
+	: func_(func)
+{
+	log(QString("--- profile [enter] %1").arg(func));
+	timer_.start();
+}
+
+Profile::~Profile()
+{
+	log(QString("--- profile [leave] %1 @ %2ms").arg(func_).arg(timer_.elapsed()));
+}
--- src/Profile.h
+++ src/Profile.h
@@ -3,28 +3,16 @@
 
 #include <QElapsedTimer>
 #include <QString>
-#include <QDebug>
 
 class Profile {
 private:
 	QString func_;
 	QElapsedTimer timer_;
-	void log(QString const &s)
-	{
-		qDebug() << s;
-	}
+	void log(QString const &s);
 public:
-	Profile(const char *func)
-		: func_(func)
-	{
-		log(QString("--- profile [enter] %1").arg(func));
-		timer_.start();
-	}
-	~Profile()
-	{
-		log(QString("--- profile [leave] %1 @ %2ms").arg(func_).arg(timer_.elapsed()));
-	}
+	Profile(const char *func);
+	~Profile();
 };
-#define PROFILE Profile _profile(Q_FUNC_INFO);
+#define PROFILE Profile _profile(Q_FUNC_INFO)
 
 #endif // PROFILE_H
