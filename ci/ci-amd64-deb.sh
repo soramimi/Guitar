ef8bf7760430b1ca1d332843126d69ee15afcef0
soramimi <fi7s-fct@asahi-net.or.jp>
2025-06-04 21:15:40 +0900

    update CI scripts

--- ci/ci-amd64-deb.sh
+++ ci/ci-amd64-deb.sh
@@ -1,15 +1,25 @@
-rm _bin -fr
-rm _build -fr
-mkdir _bin
+#!/bin/bash
+
 ruby prepare.rb
-cd packaging/deb
-mkdir _build
-cd _build
-qmake "CONFIG+=release" ../../../Guitar.pri
-make -j2
-cd ..
-cp _build/Guitar .
+
+rm -fr _build_libfiletype
+mkdir _build_libfiletype
+pushd _build_libfiletype
+qmake "CONFIG+=release" ../filetype/libfiletype.pro
+make -j8
+popd
+
+rm -fr _build_guitar
+mkdir _build_guitar
+pushd _build_guitar
+qmake "CONFIG+=release" ../Guitar.pro
+make -j8
+popd
+
+pushd packaging/deb
+cp ../../_bin/Guitar .
 ruby mk-deb.rb
 file=`./debname.rb`
 curl -T $file ftp://192.168.0.5/Public/pub/nightlybuild/
+popd
 
