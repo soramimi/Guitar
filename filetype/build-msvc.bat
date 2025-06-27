3c38b746875196ee9b6400e8dd8916dd78a4517e
soramimi <fi7s-fct@asahi-net.or.jp>
2025-06-28 06:46:31 +0900

    Add debug and release build configurations to MSVC build script

--- filetype/build-msvc.bat
+++ filetype/build-msvc.bat
@@ -3,23 +3,47 @@ call c:\vcvars\vcvars.bat
 rmdir /s /q _build
 mkdir _build
 cd _build
-C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe ../libfile.pro
+C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe "CONFIG+=debug" ../libfile.pro
 C:\Qt\Tools\QtCreator\bin\jom\jom.exe
 cd ..
 
 rmdir /s /q _build
 mkdir _build
 cd _build
-C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe ../liboniguruma.pro
+C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe "CONFIG+=release" ../libfile.pro
+C:\Qt\Tools\QtCreator\bin\jom\jom.exe
+cd ..
+
+
+rmdir /s /q _build
+mkdir _build
+cd _build
+C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe "CONFIG+=debug" ../liboniguruma.pro
 C:\Qt\Tools\QtCreator\bin\jom\jom.exe
 cd ..
 
 rmdir /s /q _build
 mkdir _build
 cd _build
-C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe ../libfiletype.pro
+C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe "CONFIG+=release" ../liboniguruma.pro
 C:\Qt\Tools\QtCreator\bin\jom\jom.exe
 cd ..
+
+
+rmdir /s /q _build
+mkdir _build
+cd _build
+C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe "CONFIG+=debug" ../libfiletype.pro
+C:\Qt\Tools\QtCreator\bin\jom\jom.exe
+cd ..
+
+rmdir /s /q _build
+mkdir _build
+cd _build
+C:\Qt\6.10.0\msvc2022_64\bin\qmake.exe "CONFIG+=release" ../libfiletype.pro
+C:\Qt\Tools\QtCreator\bin\jom\jom.exe
+cd ..
+
 
 rem cd lib
 rem lib /OUT:filetype.lib filetype.lib file.lib oniguruma.lib
