da278ea8cbd8656c537a69409efa20b3d7e2f8dc
soramimi <fi7s-fct@asahi-net.or.jp>
2026-05-03 13:48:08 +0900

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,10 @@ extern void process_test();
 
 int main(int argc, char *argv[])
 {
+	if (0) {
+		process_test();
+		return 0;
+	}
 	putenv("QT_ASSUME_STDERR_HAS_CONSOLE=1");
 	qInstallMessageHandler(logHandler);
 
